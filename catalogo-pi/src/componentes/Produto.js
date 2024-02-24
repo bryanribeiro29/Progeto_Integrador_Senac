339ba317aa1d3697e4d41be757dcd0796b048bc9
bryanribeiro29 <129908079+bryanribeiro29@users.noreply.github.com>
2024-02-23 22:12:05 -0300

    commit implementação da busca de api

--- catalogo-pi/src/componentes/Produto.js
+++ catalogo-pi/src/componentes/Produto.js
@@ -1,7 +1,28 @@
 
 
-export default function Produto(){
-        return (
-            <div></div>
-        )
+export default function Produto() {
+    return (
+        <div className="coll-3">
+            <div style={{ width: "100%", height: 200, backgroundColor: "gray" }}></div>
+            <div className="card-body">
+                <h2>{props.nome}</h2>
+                <p>{props.descricao}</p>
+                <div className="d-flex justify-content-between aling-items-center">
+                    {
+                        props.disponivel === false
+                            ? <span className="badge text-bg-secondary">Indisponivel</span>
+                            : null
+                    }
+                    {
+                        props.novidade == true
+                        ? <span className="badge text-bg-success">Novidade</span>
+                        : null
+                    }
+                </div>
+                <div className="text-end mt-3">
+                    <h3 className="text-body-secundary">{props.Preco}</h3>
+                </div>
+            </div>
+        </div>
+    )
 }
--- catalogo-pi/src/pages/index.js
+++ catalogo-pi/src/pages/index.js
@@ -1,5 +1,30 @@
+import Produto from "@/componentes/Produto";
+import axios from "axios";
+import { useState } from "react";
+
 export default function Home() {
+
+  const [listaProduto, setListaPRoduto] = useState([])
+
+  axios.get('http://localhost:3000')
+    .then(resp => setListaPRoduto(resp.data))
+
   return (
-    <div>Hola</div>
-    );
+
+    <div className="contairner-fluid mt-2">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-ms-3 g-3">
+        {
+          listaProduto.map((dado, index) => <Produto
+            key={index}
+            nome={dado.nome} 
+            descricao={dado.descricao}
+            disponivel={dado.disponivel} 
+            novidade={dado.novidade}
+            preco={dado.preco} />)
+        }
+
+      </div>
+    </div>
+
+  );
 }
