7a641eb1df639523422e6065cbe67b462946bea9
bryanribeiro29 <129908079+bryanribeiro29@users.noreply.github.com>
2024-02-26 22:22:19 -0300

    aprimoramento do layout

--- catalogo-pi/src/componentes/Produto.js
+++ catalogo-pi/src/componentes/Produto.js
@@ -1,9 +1,9 @@
 
 
-export default function Produto() {
+export default function Produto(props) {
     return (
         <div className="coll-3">
-            <div style={{ width: "100%", height: 200, backgroundColor: "gray" }}></div>
+            <div style={"/assets/Bmw m3.png"}{{ width: "100%", height: 200, backgroundColor: "gray" }}></div>
             <div className="card-body">
                 <h2>{props.nome}</h2>
                 <p>{props.descricao}</p>
@@ -26,3 +26,14 @@ export default function Produto() {
         </div>
     )
 }
+
+
+<div className="col-3">
+            <div className="card shadow-sm p-4">
+                <div className="text-center ">
+                <Image src={"/assets/Air-Jordan-4.png"} width={250} height={150} />.
+
+
+
+
+                Apagar isso aqui em cima após alterar o que for feito.
--- catalogo-pi/src/pages/index.js
+++ catalogo-pi/src/pages/index.js
@@ -1,20 +1,23 @@
 import Produto from "@/componentes/Produto";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [listaProduto, setListaPRoduto] = useState([])
-
-  axios.get('http://localhost:3000')
-    .then(resp => setListaPRoduto(resp.data))
-
+  const [listaProduto, setListaProduto] = useState([])
+  
+    useEffect(() => {
+      axios.get('https://localhost:7282/api/Produto/ListaAsync')
+      .then(resp => setListaProduto(resp.data))
+      console.log(listaProduto)
+    }, [])
   return (
 
     <div className="contairner-fluid mt-2">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-ms-3 g-3">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-ms-3 g-3">
         {
-          listaProduto.map((dado, index) => <Produto
+          listaProduto.map((dado, index) => 
+          <Produto
             key={index}
             nome={dado.nome} 
             descricao={dado.descricao}
